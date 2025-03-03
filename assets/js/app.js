31fe571fdc3862c9d8063cf5c3360bbd4e532046
khushboo-590 <khushbook4979@gmail.com>
2025-03-03 09:41:38 +0530

    section-2 responive

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,3 +5,17 @@ menuIcon.addEventListener("click", () => {
     let activeNav = menuList.hasAttribute("class", "show");
     if (activeNav) { menuIcon.classList.toggle("active") };
 })
+
+const backToTopButton = document.getElementById("backToTop");
+
+window.addEventListener("scroll", function () {
+    if (window.scrollY > 300) {
+        backToTopButton.style.display = "block";
+    } else {
+        backToTopButton.style.display = "none";
+    }
+});
+
+backToTopButton.addEventListener("click", function () {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+});
